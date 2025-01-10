68c82e3af2475b14d78767500c8cfc849e41e11c
Riya Ojha <rojha5250@gmail.com>
2025-01-10 16:22:07 +0530

    User Service Done

--- src/Utilities/Constants/ErrorMessage.java
+++ src/Utilities/Constants/ErrorMessage.java
@@ -8,4 +8,5 @@ public class ErrorMessage {
     public static final String INVALID_AGE = "Age must be a positive number and less than 120.";
     public static final String INVALID_PHONE_NUMBER = "Phone number must be exactly 10 digits.";
     public static final String ERROR_WHILE_REGISTER = "Error while registering the user.";
+    public static final String ERROR_WHILE_LOGIN = "Incorrect credentials";
 }
