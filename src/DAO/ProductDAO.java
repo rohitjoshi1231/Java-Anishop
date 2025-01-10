d19c3c45c66a3937309829c5f6c4b162df56ee29
Riya Ojha <rojha5250@gmail.com>
2025-01-10 11:36:14 +0530

    added the functionality of product

--- src/DAO/ProductDAO.java
+++ src/DAO/ProductDAO.java
@@ -1,5 +1,10 @@
 package DAO;
 
 public class ProductDAO {
-    
+
+    public void addProduct(String productName, String productDescription, int productStock, double productPrice) {
+
+        System.out.println("Function not implemented yet.");
+    }
+
 }
--- src/Service_layer/UserService.java
+++ src/Service_layer/UserService.java
@@ -5,14 +5,15 @@ import DAO.UserDAO;
 public class UserService {
     private final UserDAO userDAO = new UserDAO();
 
-    public void registerUser(
-        
+    public void registerUser(     
             String emailId,
             String password,
             String name,
             char gender,
             int age,
             String phoneNumber) {
-        userDAO.registerUser( emailId, password, name, gender, age, phoneNumber);
+        userDAO.registerUser(emailId, password, name, gender, age, phoneNumber);
+        
+        
     }
 }
