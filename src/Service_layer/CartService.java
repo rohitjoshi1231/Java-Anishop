d28bd3056f850e0973ebe5e5e84204fbf02a8542
Riya Ojha <rojha5250@gmail.com>
2025-01-11 10:55:37 +0530

    test 1.5

--- src/Service_layer/CartService.java
+++ src/Service_layer/CartService.java
@@ -7,38 +7,6 @@ import DAO.UserDAO;
 import Utilities.Constants.ErrorMessage;
 
 public class CartService {
-    public void addCart(int number) {
-        UserDAO userDAO = new UserDAO();
-        ResultSet res1 = null; // result set ki value
-          try {
-            // Fetch user details
-            res1 = userDAO.loginUser();
-            if (res1 == null) {
-                System.out.println("No data found or an error occurred while fetching user details.");
-                return;
-            }
 
-            String fetchedEmailId = "" ;
-            String fetchedPassword = "";
-                     
-            while (res1.next()) {
-            fetchedEmailId = res1.getString("EmailId");
-            fetchedPassword = res1.getString("Password");
-            }
-
-            if (fetchedEmailId.equals(emailId) && fetchedPassword.equals(password)) {
-                System.out.println("Match found for: " + fetchedEmailId);
-            } else {
-                System.out.println(ErrorMessage.ERROR_WHILE_LOGIN);
-            }
-
-        } catch (SQLException e) {
-            System.out.println("Error while processing user details: " + e.getMessage());
-        
-
-
-        
-
-    }
     
 }
--- src/UI_Layer/CartDisplay.java
+++ src/UI_Layer/CartDisplay.java
@@ -6,7 +6,7 @@ public class CartDisplay {
     public static void main(String[] args) {
         System.out.println("Welcome to the User Cart");
         CartService service = new CartService();
-        service.addCart(1);
+        // service.addCart(1);
     }
 
 }
