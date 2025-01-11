54ba613ed34a2fdd8ac66435ac954a4cf7f33af7
rohitjoshi1231 <rash5932q@gmail.com>
2025-01-11 15:42:06 +0530

    Your commit message describing the changes

--- src/DAO/CartDAO.java
+++ src/DAO/CartDAO.java
@@ -8,19 +8,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-<<<<<<< HEAD
-=======
-
->>>>>>> fb138f2d9a1ba756c553217ed8eb84711db4f929
 
 public class CartDAO {
     private static final Connection conn = DBConnection.connect();
 
+    // Method to show a selected product by productId
     public static ResultSet showSelectedProduct(int productId) {
         ResultSet res1 = null;
         try (conn) {
+            assert conn != null;
             PreparedStatement preparedStatement = conn.prepareStatement(SqlQueries.SELECT_PRODUCT_WITH_ID);
-
             preparedStatement.setInt(1, productId);
             res1 = preparedStatement.executeQuery();
         } catch (SQLException e) {
@@ -29,37 +26,24 @@ public class CartDAO {
         return res1; // Return the result set (can be null if an error occurred)
     }
 
-<<<<<<< HEAD
-    public static int addCart(int productId, int Quantity) {
-        try (Connection conn = DBConnection.connect()) {
-=======
-    public int addCart(int productId, int Quantity) {
+    // Method to add a product to the cart
+    public static void addCart(int productId, int Quantity) {
         try (conn) {
->>>>>>> fb138f2d9a1ba756c553217ed8eb84711db4f929
             assert conn != null;
             ResultSet productDetails = showSelectedProduct(productId);
             PreparedStatement preparedStatement = conn.prepareStatement(SqlQueries.INSERT_CART);
             while (productDetails.next()) {
-
                 int productId1 = productDetails.getInt("ProductId");
-                // String productName = productDetails.getString("ProductName");
-                // String productDescription = productDetails.getString("ProductDescription");
-                // int productstock = productDetails.getInt("ProductStock");
                 String productPrice = productDetails.getString("ProductPrice");
 
                 preparedStatement.setInt(1, productId1);
                 preparedStatement.setInt(2, Quantity);
                 preparedStatement.setString(3, productPrice);
-
             }
 
-            return preparedStatement.executeUpdate();
-
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println(ErrorMessage.ERROR_WHILE_ADDING_PRODUCT_IN_CART);
         }
-        return 0;
     }
-
-
 }
--- src/Service_layer/CartService.java
+++ src/Service_layer/CartService.java
@@ -1,43 +1,36 @@
 package Service_layer;
 
-<<<<<<< HEAD
-public class CartService {
-    public static int addCart(int productId, int Quantity) {
-
-    }
-
-=======
 import Utilities.DBConnection;
 import Utilities.ValidationUtil;
+import DAO.CartDAO;
+import Utilities.Constants.ErrorMessage;
+import Utilities.Constants.SqlQueries;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import DAO.CartDAO;
-import Utilities.Constants.ErrorMessage;
-import Utilities.Constants.SqlQueries;
-
 public class CartService {
-     private static final Connection conn = DBConnection.connect();
-
+    private static final Connection conn = DBConnection.connect();
     private final static ValidationUtil validation = new ValidationUtil();
-        private final static CartDAO cart = new CartDAO();
-    
-        public static void addCart(int productId, int Quantity) {
-            if (validation.validateQuantity(Quantity)) {
-                cart.addCart(productId, Quantity);
-            } else {
-                System.out.println(ErrorMessage.ERROR_WHILE_ADDING_PRODUCT_IN_CART);
-            }
+
+    // Method to add a product to the cart
+    public static void addCart(int productId, int Quantity) {
+        if (validation.validateQuantity(Quantity)) {
+            CartDAO.addCart(productId, Quantity);  // Call CartDAO to add the product to the cart
+        } else {
+            System.out.println(ErrorMessage.ERROR_WHILE_ADDING_PRODUCT_IN_CART);
         }
-    
-        public void displayCartItems() {
+    }
+
+    // Method to display items in the cart
+    public void displayCartItems() {
         try (conn) {
+            assert conn != null;
             PreparedStatement preparedStatement = conn.prepareStatement(SqlQueries.SHOW_CART_PRODUCTS);
             ResultSet res = preparedStatement.executeQuery();
-            while(res.next()){
+            while (res.next()) {
                 int CartId = res.getInt("CartId");
                 String ProductId = res.getString("ProductId");
                 String Quantity = res.getString("Quantity");
@@ -48,14 +41,8 @@ public class CartService {
                         ", ProductDescription: " + Quantity + ", PriceAtAdd: " + PriceAtAdd +
                         ", AddedAt: " + AddedAt);
             }
-
-    }catch(
-
-    SQLException e)
-    {
+        } catch (SQLException e) {
             System.out.println(ErrorMessage.ERROR_WHILE_DISPLAYING_PRODUCT_IN_CART + e);
         }
-}
->>>>>>> fb138f2d9a1ba756c553217ed8eb84711db4f929
-
+    }
 }
